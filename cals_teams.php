8b5ef8ffe51755192ae67ea9f6fca6b0964d8c46
Daniel Dropik <ddropik@wisc.edu>
2015-12-17 14:00:28 -0600

    initial commit

--- cals_teams.php
+++ cals_teams.php
@@ -8,14 +8,13 @@
    Text Domain: cals_teams
    License: GPLv3
   */
- //The one in cals.main verison
- 
-//Prevent Direct Access to this plugin
+//version from cals.main
+
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 
-
 //Define Constants
+
 if(!defined('CT_PLUGIN_BASE_FILE')){
   define('CT_PLUGIN_BASE_FILE',__FILE__);
 }
@@ -31,7 +30,6 @@ if(!defined('CT_PLUGIN_URL')){
 //Variables
 $prefix = 'calsteams_';// 
 
-//Functions
 function create_cals_teams_post_type() {
 
   register_post_type( 'cals_team_members',
@@ -59,7 +57,6 @@ function create_cals_teams_post_type() {
 add_action( 'init', 'create_cals_teams_post_type' );
 
 
-
 function create_cals_teams_taxonomies(){
 
   register_taxonomy('cals_groups','cals_team_members',
@@ -83,57 +80,197 @@ function create_cals_teams_taxonomies(){
 }
 add_action( 'init', 'create_cals_teams_taxonomies');
 
+
+
+
 //fields. Prefixed by "field_"
 $field_office_location = array(
-	'name'=>'Offie Location',
-	'desc'=>'',
-	'id'=>$prefix . 'office_location',
-	'type'=>'text',
-	'std'=>'office location',//default value
-	);
+  'name'=>'Offie Location',
+  'desc'=>'This is where the office is.',
+  'id'=>$prefix . 'office_location', //corresponds to input field name & id
+  'type'=>'text',
+  'std'=>'office location',//default value
+  );
+
+//field2
+$field_office_location2 = array(
+  'name'=>'Offie Location2',
+  'desc'=>'This is where the office is.',
+  'id'=>$prefix . 'office_location_2', //corresponds to input field name & id
+  'type'=>'text',
+  'std'=>'office location_2',//default value
+  );
+
 
 //metabox
 $mbox = array(
   'id'=>'cals_teams_mbox_0',//HTML ID
   'title'=>'Team Member Data',//MetaBox Title
   'screen'=>'cals_team_members',//custom post type slug
-  'context'=>'side',//display location
+  'context'=>'normal',//display location
   'priority'=>'default',
-  'fields'=>array($field_office_location),
+  'fields'=>array($field_office_location,$field_office_location2),
   );
 
+
+
 logit($mbox,'$mbox: ');
+
 
 //add meta boxes
 function add_meta_boxes_cals_team_members($post){
 
-  //add_meta_box('cals_teams_cmb_OL', __('Office Location','cals_teams'), 'OL_cb','cals_team_members','side');
-  
-  add_meta_box($mbox['id'],$mbox['title'],'calsteams_cb',$mbox['screen'],$mb['context'],$mb['priority']);
+  global $mbox;
+
+  add_meta_box($mbox['id'],$mbox['title'], 'calsteams_buildform_cb',$mbox['screen'],$mbox['context']);
 }
 add_action( 'add_meta_boxes', 'add_meta_boxes_cals_team_members' );
 
 
-//callback generates form markup
-function calsteams_cb($post){
+//generates metabox markup on admin
+function calsteams_buildform_cb($post){
 
-  $OL_stored_meta = get_post_custom($post->ID); //get post ID
+  global $mbox, $post;//bring in these variables from global scope
+
+  $mbox_data = get_post_custom($post->ID); //get array containing metabox custom fields
+
+  logit($mbox_data,'$mbox_data: ');
+  //$OL_stored_meta = get_post_custom($post->ID); //get post ID
   //logit($OL_stored_meta,'$OL_stored_meta: ');
+
   
-  $text = isset( $OL_stored_meta['mb_OL_input'] ) ? esc_attr($OL_stored_meta['mb_OL_input'][0] ) : "";
+  //$text = isset( $OL_stored_meta['mb_OL_input'] ) ? esc_attr($OL_stored_meta['mb_OL_input'][0] ) : "";
+  //$text = isset( $mbox_data['the_input_id_prop'] ) ? esc_attr($mbox_data['the_input_id_prop'][0] ) : "";
 
-  wp_nonce_field( 'cals_teams_update_OL', 'OL_nonce');
+  //wp_nonce_field( 'cals_teams_update_OL', 'OL_nonce');
+  wp_nonce_field( 'calsteams_update_field', 'calsteams_nonce');
 
-  ?>
-  <p>
-    <label><?php _e('Office Locale','cals_teams') ?></label>
-    <input type="text" id="mb_OL_input" name="office_location" value="<?php echo $text; ?>"/>
-  </p>
-  <?php
+  $temp_fields = $mbox['fields'];
+  $temp_field_id = $temp_fields['id'];
+
+  logit($temp_fields,'$temp_fields: ');
+  logit($temp_field_id,'$temp_field_id: ');
+  logit($post->ID,'$post->ID: ');
+
+  echo '<table class="form-table">';
+
+
+  foreach ($mbox['fields'] as $field) {
+
+    $meta = get_post_meta($post->ID,$field['id'],true); //get meta-box data for current field
+
+    echo '<tr>',
+                '<th style="width:20%"><label for="', $field['id'], '">', $field['name'], '</label></th>',
+                '<td>';
+
+                switch ($field['type']) {
+                  case 'text':
+                    echo '<input type="text" name="', $field['id'], '" id="', $field['id'], '" value="', $meta ? $meta : $field['std'], '" size="30" style="width:97%" />', '<br />', $field['desc'];
+                    break;
+
+                  default:
+                    echo 'uh oh, default case!';
+                }
+
+          echo '</td>',
+            '</tr>';
+  }
+  echo '</table>';
 }
 
+//save metabox form data
+function calsteams_mbox_save($post_id){
+  // Checks save status
+  global $mbox;
+
+  $is_autosave = wp_is_post_autosave( $post_id );
+  $is_revision = wp_is_post_revision( $post_id );
+  $is_valid_nonce = ( isset( $_POST['calsteams_nonce'] ) ) && wp_verify_nonce($_POST['calsteams_nonce'],'calsteams_update_field') ? 'true' : 'false';
+
+  // Exits script depending on save status
+  if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+    return;
+  }
+
+  //Real foreach, temporarily commented out
+  foreach ($mbox['fields'] as $field) {
+
+      $input_id = $field['id'];//get the current item's input id property
+
+      // Checks for input and sanitizes/saves if needed
+      if( isset( $_POST[ $input_id ] ) ) {
+
+        update_post_meta( $post_id, $input_id, sanitize_text_field( $_POST[ $input_id ] ) );
+      }
+  }
+}
+add_action('save_post', 'calsteams_mbox_save');
 
 
+/*//save metabox data
+function meta_box_OL_save($post_id){
+    // Bail if we're doing an auto save
+  if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+
+    // if our nonce isn't there, or we can't verify it, bail
+  if( !isset( $_POST['OL_nonce'] ) || !wp_verify_nonce( $_POST['OL_nonce'], 'cals_teams_update_OL' ) ) return;
+
+    // if our current user can't edit this post, bail
+  if( !current_user_can( 'edit_post' ) ) return;
+
+  if( isset( $_POST['mb_OL_input'] ) )
+    update_post_meta( $post_id, 'mb_OL_input', esc_attr( $_POST['mb_OL_input'] ) );
+
+}
+
+add_action('save_post','meta_box_OL_save' );*/
+
+function template_chooser($template){
+  $post_id = get_the_ID();
+
+  if(get_post_type($post_id) != 'cals_team_members'){
+
+    return $template;
+  }
+
+  if(is_single()){
+    return ct_get_template_hierarchy('single');
+  }
+}
+add_filter('template_include','template_chooser');
 
 
-?>
+/**
+ * Get the custom template if is set
+ *
+ * @since 1.0
+ */
+ 
+function ct_get_template_hierarchy( $template ) {
+ 
+    // Get the template slug
+    $template_slug = rtrim( $template, '.php' );//single
+    $template = $template_slug . '.php'; //single.php
+
+    //logit($template,'$template: ');
+    //logit($template_slug,'$template_slug: ');
+
+    //$locate = locate_template( array( 'plugin_templates/single.php' ) );
+    //$locateString = 'plugin_template/' . $template;
+    //logit($locateString,'$locateString: ');
+    //logit($locate,'$locate: ');
+ 
+    // Check if a custom template exists in the theme folder, if not, load the plugin template file
+    if ( $theme_file = locate_template( array( 'cals_teams_templates/' . $template ) ) ) {
+        $file = $theme_file;
+        logit($file,'$file: ');
+
+    }
+    else {
+        $file = CT_PLUGIN_BASE_DIR . '/includes/templates/' . $template;
+    }
+ 
+    //return apply_filters( 'rc_repl_template_' . $template, $file );
+    return $file;
+}
+add_filter( 'template_include', 'template_chooser' );
