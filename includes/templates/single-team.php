df7471af4e5075d29c4cae469b8c5e1d261be571
Daniel Dropik <ddropik@wisc.edu>
2016-01-21 09:00:26 -0600

    comment out nav-next/previous
    Removes nav-next/previous because this is expected to be very different from theme to theme.

--- includes/templates/single-team.php
+++ includes/templates/single-team.php
@@ -122,7 +122,7 @@
 				comments_template();
 			endif;
 
-			// Previous/next post navigation.
+/*			// Previous/next post navigation.
 			the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
 					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
@@ -130,7 +130,7 @@
 				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
 					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-			) );
+			) );*/
 
 		// End the loop.
 		endwhile;
