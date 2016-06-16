d1e4d18754a8671824aece8f8654432ef9d17129
Daniel Dropik <ddropik@wisc.edu>
2016-06-16 15:25:48 -0500

    remove first & last name if title contains name on single

--- includes/scripts/cals_teams.js
+++ includes/scripts/cals_teams.js
@@ -4,3 +4,22 @@ if(document.querySelector('.bricklayer')){
 	var bricklayer = new Bricklayer(document.querySelector('.bricklayer'));
 
 }
+
+// Execute js on page single-team.php (from plugin template)
+if($('body').is(".cals_team-single-team")){
+
+	//get length of .entry-title contents
+	var titleLen = $("h1.entry-title").html().length;
+
+	//if .entry-title has contents remove first-name and last-name
+	if( titleLen > 0 ){
+
+		$(".short-vals-container").each( function(){
+		
+			$("span.team-item-label:contains(First Name)").parent().css("display","none");
+
+			$("span.team-item-label:contains(Last Name)").parent().css("display","none");
+
+		});
+	}
+}
