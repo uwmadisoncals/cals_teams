8228e27054f9364031ada57cd113600b1b6ba676
Kieran Furey-King <kfureyking@wisc.edu>
2016-12-09 09:12:43 -0600

    new headshot display

--- includes/templates/archive-team.php
+++ includes/templates/archive-team.php
@@ -23,14 +23,13 @@ get_header(); ?>
 <div class="site-content-inner">
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main cals_teams archive_team" role="main">
-		<?php
+			<div class="cf pagePadding">
 
+		<?php
 			include( WP_PLUGIN_DIR . '/cals_teams/includes/data/cals_teams_fields.php' ); //include Metabox and Metabox field group data
 
-		
 		$mbox_fields = $mbox['fields'];//Meta data for field groups
 		//logit($mbox_fields,'$mbox_fields');
-		
 
 		$args = array('post_type'=>'team',
 					  'orderby'=>'menu_order',
@@ -39,7 +38,7 @@ get_header(); ?>
 		$cals_teams_query = new WP_Query($args);//Instantiate New WP Query Object
 		//logit($cals_teams_query,'$cals_teams_query');
 		//logit($cals_teams_query->post->ID,'$cals_teams_query id: ');
-		
+
 		$cals_teams_obj = new Cals_Teams($mbox); //Instantiate Cals_Teams object
 		//logit($cals_teams_obj,'$cals_teams_obj: ');
 
@@ -47,19 +46,19 @@ get_header(); ?>
 
 		<?php if ( $cals_teams_query->have_posts() ) : ?>
 
-			<header>
+			<header class="entry-header">
 				<?php
 					//the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 				<!--<h1 class="page-title" >Lab Members</h1>-->
 
-				<h1 class="page-title"><?php echo ct_settings_options_get(); ?></h1>
+				<h1 class="entry-title"><?php echo ct_settings_options_get(); ?></h1>
 
 			</header><!-- .page-header -->
 
-			<div class="member-grouping bricklayer">
-			
+			<div class="member-grouping">
+
 			<?php
 			// Start the Loop.
 			while ( $cals_teams_query->have_posts() ) : $cals_teams_query->the_post();
@@ -68,96 +67,76 @@ get_header(); ?>
 
 			//logit($id,'$id: ');
 			//logit(get_post_meta($id),'gpmid: ');
-			
+
 				$all_meta = get_post_meta($id);
 				//logit($all_meta,'$all_meta: ');
 
-				$name_prefix = get_post_meta($id, 'calsteams_name_prefix')[0];
-				$first_name = get_post_meta($id, 'calsteams_first_name')[0];
-				$last_name = get_post_meta($id, 'calsteams_last_name')[0];
 				$pro_title = get_post_meta($id, 'calsteams_professional_title')[0];
+				$description = get_post_meta($id, 'calsteamswysiwygdesc')[0];
 				$image_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 				//logit($image_url);
 
 			?>
 				<div class="member_wrapper">
 
-				<?php if($image_url){ ?>
-
-					<a href="<?php esc_url( the_permalink() ); ?>">
-					<div class="newImg" style="background: url(<?php echo $image_url ?>) no-repeat center center; background-size: cover;"></div>
+					<a href="<?php esc_url( the_permalink() ); ?>" style="background: url('<?php if($image_url){
+						echo $image_url ;
+					} else {
+						echo plugins_url() . '/cals_teams/includes/images/calsteams_placeholder.png' ;
+					} ?>') no-repeat center center; background-size: cover; display:block; height:300px;">
+						<span><?php echo the_title() ?></span>
 					</a>
-
-				<?php }else{ ?>
-					<a href="<?php esc_url( the_permalink() ); ?>">
-					<div class="newImg" style="background: url('<?php echo plugins_url() . '/cals_teams/includes/images/calsteams_placeholder.png' ?>') no-repeat center center; background-size: cover;"></div>
-					</a>
-					
-				<?php } ?>
-
-				
 
 				<div class="member_info_wrapper">
 
+										<div class="member-body">
+
+											<div class="member-data-wrapper">
+											<?php
+										//using foreach
+										foreach ($mbox_fields as $key => $value) {
+											$field_id = $value['id'];//field id name
+											$allowed_fields = array('calsteams_professional_title','calsteams_office_location','calsteams_email','calsteams_phone');
+
+											if(!empty(get_post_meta($id,$field_id)[0]) && in_array($field_id,$allowed_fields) ){
+
+												?>
+												<div class="member-data-field <?php echo $field_id; ?>">
+													<span class="field-label"></span>
+													<span class="field-data"><?php echo get_post_meta($id,$field_id)[0]; ?></span>
+												</div>
+
+												<?php
+
+											}
+
+										}// END foreach
+										?>
+
+										</div><!-- END .member-data-wrapper -->
+
+										</div><!--END .member-body -->
+
+
+
 					<div class="member-heading-wrapper">
-						<a class ="member" href="<?php esc_url(the_permalink()) ?>"><?php echo $name_prefix . ' ' . $first_name . ' ' . $last_name;  ?></a><br/>
-						<span class="protitle"><?php echo $pro_title; ?></span>
-					</div>
 
-					<div class="member-body">
-					<?php 
-						if(has_post_thumbnail()) : ?>
-
-
-
-
-<!-- 							<div class="member-image-wrapper" style="padding-top:20px;">
-								<a href="<?php esc_url(the_permalink()) ?>">
-									<?php the_post_thumbnail('medium',array('class'=>'member-thumbnail')); ?>
-								</a>
-							</div> -->
-
-						<?php else : ?>
-
-<!-- 							<div class="member-image-wrapper" style="padding-top:20px;">
-								<a href="<?php esc_url(the_permalink()) ?>">
-									<img class="member-thumbnail" alt="person placeholder image" src="<?php echo plugins_url() . '/cals_teams/includes/images/calsteams_placeholder.png'  ?>" width="150" height="150" />
-								</a>
-							</div>
- -->							
-						<?php endif; ?>
-
-						<div class="member-data-wrapper">
+						<p class="description">
 						<?php
-					//using foreach
-					foreach ($mbox_fields as $key => $value) {
-						$field_id = $value['id'];//field id name
-						$allowed_fields = array('calsteams_office_location','calsteams_phone','calsteams_email');
+							$length = 200; // characters
+							if (strlen($description) >= $length) {
+								echo substr($description, 0, $length) . '...';
+							} else {
+								echo $description;
+							}
+						?></p>
 
-						if(!empty(get_post_meta($id,$field_id)[0]) && in_array($field_id,$allowed_fields) ){
-
-							?>
-							<div class="member-data-field <?php echo $field_id; ?>">
-								<span class="field-label"></span>
-								<span class="field-data"><?php echo get_post_meta($id,$field_id)[0]; ?></span>
-							</div>
-
-							<?php
-							
-						}
-						
-						
-					}// END foreach
-					?>
-
-					</div><!-- END .member-data-wrapper -->
-
-					</div><!--END .member-body -->
+					</div>
 
 				</div><!-- END .member_info_wrapper  -->
 
 			</div><!-- END .member_wrapper -->
-				
+
 
 				<?php
 
@@ -181,7 +160,7 @@ get_header(); ?>
 
 		endif;
 		?>
-
+	</div><!-- cf pagePadding -->
 		</main><!-- .site-main -->
 	</section><!-- .content-area -->
 	</div><!-- .site-content-inner -->
